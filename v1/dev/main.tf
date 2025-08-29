568938b429b4e69369c0d7dcef9aeb7f197bf5df
Andre Kueda <kuedaa@yahoo.ca>
2025-08-29 22:49:24 +0000

    Create private and public subnets

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -1,10 +1,36 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   
-  #enable_dns_support    = true
-  #enable_dns_hostnames  = true
+  tags = {
+    Name = "VPC"
+    Env = "${var.env}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  count = var.number_subnet
+  cidr_block = var.cidr_private_subnet[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "${var.env}-main"
+    Name = "Private-Subnet-AZ${count.index}"
+    Env = "${var.env}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  count = var.number_subnet
+  cidr_block = var.cidr_public_subnet[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "Public-Subnet-AZ${count.index}"
+    Env = "${var.env}"
   }
 }
--- v1/dev/variables.tf
+++ v1/dev/variables.tf
@@ -8,5 +8,18 @@ variable "cidr_block" {
 }
 
 variable "env" {
-  default = "dev"
+  default = "Dev"
+}
+
+
+variable "number_subnet" {
+  default = 3
+}
+
+variable "cidr_private_subnet" {
+  default =  ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+}
+
+variable "cidr_public_subnet" {
+  default =  ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19"]
 }
